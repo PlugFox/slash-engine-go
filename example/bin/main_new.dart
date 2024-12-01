f9f052ac30878c8c9c891a6db88c806a866f9a16
Plague Fox <plugfox@gmail.com>
2024-12-01 21:59:49 +0400

    Add missing methods to dart

--- example/bin/main_new.dart
+++ example/bin/main_new.dart
@@ -93,17 +93,50 @@ typedef _GetWorldPtrDart = ffi.Pointer<_WorldStruct> Function();
 typedef _GetObjectPtrC = ffi.Pointer<_ObjectStruct> Function(ffi.Int32 id);
 typedef _GetObjectPtrDart = ffi.Pointer<_ObjectStruct> Function(int id);
 
+// UpsertObject function
+typedef _UpsertObjectC = ffi.Void Function(ffi.Pointer<_ObjectStruct>);
+typedef _UpsertObjectDart = void Function(ffi.Pointer<_ObjectStruct>);
+
+// UpsertObjects function
+typedef _UpsertObjectsC = ffi.Void Function(
+    ffi.Pointer<_ObjectStruct>, ffi.Int32);
+typedef _UpsertObjectsDart = void Function(ffi.Pointer<_ObjectStruct>, int);
+
+// AddImpulse function
+typedef _AddImpulseC = ffi.Void Function(ffi.Int32, _VectorStruct, ffi.Double);
+typedef _AddImpulseDart = void Function(int, _VectorStruct, double);
+
+// SetVelocity function
+typedef _SetVelocityC = ffi.Void Function(ffi.Int32, _VectorStruct);
+typedef _SetVelocityDart = void Function(int, _VectorStruct);
+
+// SetPosition function
+typedef _SetPositionC = ffi.Void Function(ffi.Int32, _VectorStruct);
+typedef _SetPositionDart = void Function(int, _VectorStruct);
+
+// SetAnchor function
+typedef _SetAnchorC = ffi.Void Function(ffi.Int32, _VectorStruct);
+typedef _SetAnchorDart = void Function(int, _VectorStruct);
+
+// RemoveObject function
+typedef _RemoveObjectC = ffi.Void Function(ffi.Int32);
+typedef _RemoveObjectDart = void Function(int);
+
+// RemoveObjects function
+typedef _RemoveObjectsC = ffi.Void Function(ffi.Pointer<ffi.Int32>, ffi.Int32);
+typedef _RemoveObjectsDart = void Function(ffi.Pointer<ffi.Int32>, int);
+
+// FreeImpulsePtr function
+typedef _FreeImpulsePtrC = ffi.Void Function(ffi.Pointer<_ImpulseStruct>);
+typedef _FreeImpulsePtrDart = void Function(ffi.Pointer<_ImpulseStruct>);
+
 // FreeObjectPtr function
 typedef _FreeObjectPtrC = ffi.Void Function(ffi.Pointer<_ObjectStruct>);
 typedef _FreeObjectPtrDart = void Function(ffi.Pointer<_ObjectStruct>);
 
 // FreeWorldPtr function
-typedef _FreeWorldPtrC = ffi.Void Function(
-  ffi.Pointer<_WorldStruct>,
-);
-typedef _FreeWorldPtrDart = void Function(
-  ffi.Pointer<_WorldStruct>,
-);
+typedef _FreeWorldPtrC = ffi.Void Function(ffi.Pointer<_WorldStruct>);
+typedef _FreeWorldPtrDart = void Function(ffi.Pointer<_WorldStruct>);
 
 // Utility function to open the shared library
 ffi.DynamicLibrary _openEngineLib() {
@@ -141,6 +174,27 @@ class SlashEngine {
             lib.lookupFunction<_GetWorldPtrC, _GetWorldPtrDart>('GetWorldPtr'),
         _getObjectPtrDart = lib
             .lookupFunction<_GetObjectPtrC, _GetObjectPtrDart>('GetObjectPtr'),
+        _upsertObjectDart = lib
+            .lookupFunction<_UpsertObjectC, _UpsertObjectDart>('UpsertObject'),
+        _upsertObjectsDart =
+            lib.lookupFunction<_UpsertObjectsC, _UpsertObjectsDart>(
+                'UpsertObjects'),
+        _addImpulseDart =
+            lib.lookupFunction<_AddImpulseC, _AddImpulseDart>('AddImpulse'),
+        _setVelocityDart =
+            lib.lookupFunction<_SetVelocityC, _SetVelocityDart>('SetVelocity'),
+        _setPositionDart =
+            lib.lookupFunction<_SetPositionC, _SetPositionDart>('SetPosition'),
+        _setAnchorDart =
+            lib.lookupFunction<_SetAnchorC, _SetAnchorDart>('SetAnchor'),
+        _removeObjectDart = lib
+            .lookupFunction<_RemoveObjectC, _RemoveObjectDart>('RemoveObject'),
+        _removeObjectsDart =
+            lib.lookupFunction<_RemoveObjectsC, _RemoveObjectsDart>(
+                'RemoveObjects'),
+        _freeImpulsePtrDart =
+            lib.lookupFunction<_FreeImpulsePtrC, _FreeImpulsePtrDart>(
+                'FreeImpulsePtr'),
         _freeObjectPtrDart =
             lib.lookupFunction<_FreeObjectPtrC, _FreeObjectPtrDart>(
                 'FreeObjectPtr'),
@@ -152,6 +206,15 @@ class SlashEngine {
   final _RunDart _runDart;
   final _GetWorldPtrDart _getWorldPtrDart;
   final _GetObjectPtrDart _getObjectPtrDart;
+  final _UpsertObjectDart _upsertObjectDart;
+  final _UpsertObjectsDart _upsertObjectsDart;
+  final _AddImpulseDart _addImpulseDart;
+  final _SetVelocityDart _setVelocityDart;
+  final _SetPositionDart _setPositionDart;
+  final _SetAnchorDart _setAnchorDart;
+  final _RemoveObjectDart _removeObjectDart;
+  final _RemoveObjectsDart _removeObjectsDart;
+  final _FreeImpulsePtrDart _freeImpulsePtrDart;
   final _FreeObjectPtrDart _freeObjectPtrDart;
   final _FreeWorldPtrDart _freeWorldPtrDart;
   final _StopDart _stopDart;
@@ -188,6 +251,51 @@ class SlashEngine {
   /// Get the current object
   ffi.Pointer<_ObjectStruct> getObjectPtr(int id) {
     return _getObjectPtrDart(id);
+  }
+
+  /// Add or update a single object
+  void upsertObject(ffi.Pointer<_ObjectStruct> object) {
+    _upsertObjectDart(object);
+  }
+
+  /// Add or update multiple objects
+  void upsertObjects(ffi.Pointer<_ObjectStruct> objects, int count) {
+    _upsertObjectsDart(objects, count);
+  }
+
+  /// Add an impulse to an object
+  void addImpulse(int id, _VectorStruct direction, double damping) {
+    _addImpulseDart(id, direction, damping);
+  }
+
+  /// Set velocity for an object
+  void setVelocity(int id, _VectorStruct velocity) {
+    _setVelocityDart(id, velocity);
+  }
+
+  /// Set position for an object
+  void setPosition(int id, _VectorStruct position) {
+    _setPositionDart(id, position);
+  }
+
+  /// Set anchor for an object
+  void setAnchor(int id, _VectorStruct anchor) {
+    _setAnchorDart(id, anchor);
+  }
+
+  /// Remove a single object
+  void removeObject(int id) {
+    _removeObjectDart(id);
+  }
+
+  /// Remove multiple objects
+  void removeObjects(ffi.Pointer<ffi.Int32> ids, int count) {
+    _removeObjectsDart(ids, count);
+  }
+
+  /// Free an impulse pointer
+  void freeImpulsePtr(ffi.Pointer<_ImpulseStruct> impulse) {
+    _freeImpulsePtrDart(impulse);
   }
 
   /// Free the given object
